d80e07fe042e1c1b5d5d49b4454bc63d414b6223
Alex Fawcett <alex.fawcett@sage.com>
2016-09-28 22:44:20 +0100

    Added Geocoder

---
+++ geocoder.js
@@ -0,0 +1,39 @@
+var GoogleMapsAPI = require('googlemaps');
+var bodyParser = require('body-parser');
+
+var publicConfig = {
+  key: '',
+  stagger_time:       1000, // for elevationPath
+  encode_polylines:   false,
+  secure:             true // use https
+};
+
+var gmAPI = new GoogleMapsAPI(publicConfig);
+
+module.exports = {
+  geocodeString: function (addressString) {
+
+    var geocodeParams = {
+      "address":    addressString,
+      "components": "components=country:GB",
+      "bounds":     "55,-1|54,1",
+      "language":   "en",
+      "region":     "uk"
+    };  
+
+    gmAPI.geocode(geocodeParams , function(err, result){
+      console.log(result.results);
+      if(result.results.length == 0){
+        throw 'TooFew'; 
+      }
+      else if(result.results.length > 1){
+        throw 'TooMany';
+      }
+      else{
+        return result.results[0];
+      }
+    });
+  },
+};
+
+
